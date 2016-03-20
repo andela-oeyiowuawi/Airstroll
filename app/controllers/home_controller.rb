4b0aee16e82fc15aded0b64ccac25c65f71ec7af
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-03-20 19:28:39 +0100

    retrive search result from database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,16 @@ class HomeController < ApplicationController
     @airports = Airport.all
   end
   def search
+    @search_result = Flight.where("from_id = ? AND to_id = ? AND dept_date = ? ",
+                flight_details[:from],flight_details[:to],flight_details[:date])
+    if @search_result.empty?
+      flash[:notice] = 'No Flights. Please search again or check The list for avaailable flight'
+    end
+    redirect_to root_path
+  end
+
+  private
+  def flight_details
+    params.require(:flight).permit(:from, :to,:date)
   end
 end
