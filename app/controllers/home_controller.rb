022dd19052b05df5a9843283237792a90e48bd94
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-03-20 20:07:33 +0100

    working on the search result

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,10 @@ class HomeController < ApplicationController
   def search
     @search_result = Flight.where("from_id = ? AND to_id = ? AND dept_date = ? ",
                 flight_details[:from],flight_details[:to],flight_details[:date])
-    if @search_result.empty?
+    if @search_result.empty? || @search_result.nil?
       flash[:notice] = 'No Flights. Please search again or check The list for avaailable flight'
     end
+    binding.pry
     redirect_to root_path
   end
 
