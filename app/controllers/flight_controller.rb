99f5fbe2622e308540c3174ac326aafc6b412046
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-03-21 22:49:44 +0100

    working on the all flight page

---
+++ app/controllers/flight_controller.rb
@@ -0,0 +1,19 @@
+class FlightController < ApplicationController
+
+  def index
+    @flights = Flight.includes(:from, :to).order(cost: :asc)
+  end
+  def search
+    @search_result = Flight.where("from_id = ? AND to_id = ? AND dept_date = ? ",
+                flight_details[:from],flight_details[:to],flight_details[:date])
+    # if @search_result.empty? || @search_result.nil?
+    #   flash[:notice] = 'No Flights. Please search again or check The list for avaailable flight'
+    # end
+    # format
+  end
+
+  private
+  def flight_details
+    params.require(:flight).permit(:from, :to,:date)
+  end
+end
