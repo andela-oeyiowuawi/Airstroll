ecbdc4e8ab88043ad903467faebd8b4497c4d1ee
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-03-23 21:51:15 +0100

    feat(view): add new search features
    add search without departure date
    
    improve search result to only return flights that the departure dates are
    greater than Time.now

--- app/controllers/flight_controller.rb
+++ app/controllers/flight_controller.rb
@@ -1,20 +1,22 @@
 class FlightController < ApplicationController
-  before_action :validate_airport, only: :search
   def index
     @airports = Airport.all
   end
   def search
     @search_result = Flight.search_flight(flight_details[:from],
-                        flight_details[:to],flight_details[:date])
+                        flight_details[:to],flight_details[:date]).available
+  end
+  def all
+    @flights = Flight.all.available
+
   end
 
-  def validate_airport
-    if flight_details[:from] == flight_details[:to]
-      redirect_to root_url,
-       notice: "Origin Airport and Departure Airport can't be the same"
-       binding.pry
-    end
-  end
+  # def validate_airport
+  #   if flight_details[:from] == flight_details[:to]
+  #     redirect_to root_url,
+  #      notice: "Origin Airport and Departure Airport can't be the same"
+  #   end
+  # end
 
   private
   def flight_details
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,13 +3,5 @@ class HomeController < ApplicationController
   def index
     @airports = Airport.all
   end
-  def search
-    @search_result = Flight.search_flight(flight_details[:from],
-                        flight_details[:to],flight_details[:date])
-  end
 
-  private
-  def flight_details
-    params.require(:flight).permit(:from, :to,:date)
-  end
 end
