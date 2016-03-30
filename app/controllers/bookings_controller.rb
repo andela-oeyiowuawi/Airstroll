8f9d2b28cb0b5c713197aba75e29646653f9fbca
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-03-31 00:24:26 +0100

    feat(mailer): add the confirmation email page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,7 @@ class BookingsController < ApplicationController
   def create
     booking = Booking.new(booking_params)
     if booking.save
+      mail_sender(booking)
       redirect_to booking_path(booking)
     else
       redirect_to new_booking_path, notice: "Booking failed. Please try again."
@@ -24,6 +25,11 @@ class BookingsController < ApplicationController
   private
   def booking_params
     params.require(:booking).permit(:no_of_passenger, :confirmation_code, :flight_id, passengers_attributes: [:name, :email])
+  end
+
+  def mail_sender(booking)
+    passengers = booking.passengers
+    passengers.each{ |passenger| PassengerMailer.confirmation(passenger).deliver_now}
   end
 
 end
