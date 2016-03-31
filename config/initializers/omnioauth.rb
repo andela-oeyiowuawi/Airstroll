6dc091fd5e85e85651be26aa66ab043e97a69dc3
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-03-31 11:50:49 +0100

    chore(omnioauth): add omniauth.rb

--- config/initializers/omnioauth.rb
+++ config/initializers/omnioauth.rb
@@ -1,4 +1,6 @@
 provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+scope: 'public_profile', info_fields: 'id,name,link'
+
 
 App secret: 48d5f0296b9ca7b521252653d07f0fac
 App Id: 1684616185127232
