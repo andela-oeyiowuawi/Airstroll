1cad32afefc1c96412422d2b82a7793ebc54aa87
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-03-31 14:40:15 +0100

    feat(user): add login option to application layout

--- config/initializers/omnioauth.rb
+++ config/initializers/omnioauth.rb
@@ -1,5 +1,5 @@
-provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-scope: 'public_profile', info_fields: 'id,name,link'
+provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],scope: 'public_profile', info_fields: 'id,name,link'
+
 
 
 App secret: 48d5f0296b9ca7b521252653d07f0fac
