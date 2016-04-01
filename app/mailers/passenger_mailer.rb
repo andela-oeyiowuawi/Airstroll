65b7f34a73207e2d9ecc90f7e1fa57405f46569d
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-04-01 16:38:14 +0100

    refactor(mailer): refactor mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,8 +1,8 @@
 class PassengerMailer < ApplicationMailer
   default from: "airstroll@noreply.com"
-  def confirmation(name,email,booking)
+  def confirmation(name, email, booking)
     @passenger = name
     @booking = booking
-    mail(to: email, subject: 'Your Flight Has been successfully Booked')
+    mail(to: email, subject: "Your Flight Has been successfully Booked")
   end
 end
