5ed43808829c83ccf0636038b7f9c001429e6c2b
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-04-01 16:57:15 +0100

    feat(user): change session variables to current_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,6 @@ class SessionsController < ApplicationController
   def create
       user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = user.id
-      session[:name] = user.name
-      session[:email] = user.email
       session[:image] = request.env['omniauth.auth'][:info][:image]
       redirect_to user_profile_path
   end
